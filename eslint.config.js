60ee2a6c8a859dc7cf14fa4a267c67be01ab6083
sun0day <sun_day500@163.com>
2024-07-10 22:02:28 +0800

    chore: bench on acorn parse and walk

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     typescript: true,
-    vue: true,
-    ignores: ['docs'],
+    ignores: ["**/dist", "**/bench/fixtures"],
   },
   {
     files: ['**/__tests__/**'],
